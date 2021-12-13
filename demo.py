1eacc25326347819ff840494808b63965022bf54
cquliuxiang <20171715@cqu.edu.cn>
2021-12-13 11:03:26 +0800

    增加可定时循环抓取不同mainkey下页面的服务性能的功能

--- demo.py
+++ demo.py
@@ -2,21 +2,33 @@
 #  java博客多进程https://blog.csdn.net/zhuyiquan/article/details/80148767   
 # 优化：如何在函数中用同一个drive多次get，且少了放cookie的步骤（难点：driver.close()关需切换tab，数据库多条插入但这只是提升此程序性能）
 from time import sleep
+import time
 import json
 from selenium import webdriver
-class TestJS: #https://www.jianshu.com/p/7b760e2db555
+from selenium.webdriver.edge.service import Service
+import pymysql
+import pandas as pd
+import numpy as np
+
+
+
+class TestJS:  # https://www.jianshu.com/p/7b760e2db555
 
     def setup(self):
-        self.opt = webdriver.EdgeOptions() # 可通过DesirdCapablilitites对象实例化options，在通过其实例化driver
+        self.opt = webdriver.EdgeOptions()  # 可通过DesirdCapablilitites对象实例化options，在通过其实例化driver
         self.opt.add_argument('--headless')
-        self.driver = webdriver.Edge(executable_path='..\driver\msedgedriver-96.0.1054.29-stable.exe',options=self.opt)
+        s=Service(r"C:\code\ykb-test\tools\msedgedriver.exe")
+        self.driver = webdriver.Edge(service=s, options=self.opt)
         self.driver.maximize_window()
         self.driver.implicitly_wait(5)
-        
+
         # 添加cookie的域名需要设置对,先访问
-        self.driver.get('https://auth.cq.gov.cn:81/sso/login?utype=0&client_id=OCTVFKMB0') #解决domain报错https://www.cnblogs.com/deliaries/p/14121204.html
-        self.driver.delete_cookie('JSESSIONID') #需删除
-        self.driver.add_cookie({'name':'JSESSIONID','value':'inspurdd426acf8d27462a9f1de5fd58cd9cca','domain':'auth.cq.gov.cn','path':'/'})
+        self.driver.get(
+            'https://auth.cq.gov.cn:81/sso/login?utype=0&client_id=OCTVFKMB0')  # 解决domain报错https://www.cnblogs.com/deliaries/p/14121204.html
+        self.driver.delete_cookie('JSESSIONID')  # 需删除
+        self.driver.add_cookie(
+            {'name': 'JSESSIONID', 'value': 'inspurdd426acf8d27462a9f1de5fd58cd9cca', 'domain': 'auth.cq.gov.cn',
+             'path': '/'})
 
     def teardown(self):
         self.driver.quit()
@@ -33,13 +45,14 @@ class TestJS: #https://www.jianshu.com/p/7b760e2db555
         """点击下一页按钮"""
         self.driver.find_element_by_xpath("//*[@id='page']/div/a[10]").click()
         sleep(3)
-    def get_performance(self,url):
+
+    def get_performance(self, url):
         """获取页面标题和部分性能数据"""
 
         self.driver.get(url)
         self.driver.execute_script("document.documentElement.scrollTop=10000")
-        #print(self.driver.get_log('performance'))
-        
+        # print(self.driver.get_log('performance'))
+
         """方法一：将js命令放在列表中逐个执行"""
         # for code in ['return document.title', 'return JSON.stringify(performance.timing)']:
         #     print(self.driver.execute_script(code))
@@ -49,11 +62,11 @@ class TestJS: #https://www.jianshu.com/p/7b760e2db555
         print(js)
         print(type(json.loads(js)))
         idict = json.loads(js)
-        print(idict.keys()) #21个
+        print(idict.keys())  # 21个
         print(len(list(idict.values())))
-        #需要用map而非print(list(idict.values())[:]-idict['navigationStart'])
-        #print(list(map(lambda x: x-idict['navigationStart'],list(idict.values())))) 发现有些时间戳为零，现在先计算再存而非直接
-        times = {} #https://zhuanlan.zhihu.com/p/82981365
+        # 需要用map而非print(list(idict.values())[:]-idict['navigationStart'])
+        # print(list(map(lambda x: x-idict['navigationStart'],list(idict.values())))) 发现有些时间戳为零，现在先计算再存而非直接
+        times = {}  # https://zhuanlan.zhihu.com/p/82981365
         times['redirctTime'] = idict['redirectEnd'] - idict['redirectStart']
         times['dnsTime'] = idict['domainLookupEnd'] - idict['domainLookupStart']
         times['ttfbTime'] = idict['responseStart'] - idict['navigationStart']
@@ -68,35 +81,35 @@ class TestJS: #https://www.jianshu.com/p/7b760e2db555
         times['allTime'] = sum(times.values())
         print(times)
         return times
-        #with open("timing2.json",'w',encoding="utf-8") as f:
+        # with open("timing2.json",'w',encoding="utf-8") as f:
         #    json.dump(js,f,indent=4,ensure_ascii=False)
 
-
-    def performance2mysql(self,url="https://zwykb.cq.gov.cn/grbs/",mysqlurl="10.236.101.17/3306/root/123456/performancedb/TB_performance2"):
-        def generate_insert_sql(tbname,dicts):
+    def performance2mysql(self, url="https://zwykb.cq.gov.cn/grbs/",
+                          mysqlurl="10.236.101.17/3306/root/123456/performancedb/TB_performance2"):
+        def generate_insert_sql(tbname, dicts):
             cols = ",".join('{}'.format(k) for k in dicts.keys())
             val_cols = ','.join('{}'.format(k) for k in dicts.values())
             sql = """INSERT INTO %s(%s) VALUES(%s)""" % (tbname, cols, val_cols)
             return sql
+
         times = self.get_performance(url)
-        times['service_url'] = "'"+url+"'" # 需要嵌套否则后面生成sql无引号
-        import pymysql
+        times['service_url'] = "'" + url + "'"  # 需要嵌套否则后面生成sql无引号
         mysqlurl = mysqlurl.split("/")
-        conn = pymysql.connect(host=mysqlurl[0],port=int(mysqlurl[1]),user=mysqlurl[2],passwd=mysqlurl[3],db=mysqlurl[4])
+        conn = pymysql.connect(host=mysqlurl[0], port=int(mysqlurl[1]), user=mysqlurl[2], passwd=mysqlurl[3],
+                               db=mysqlurl[4])
         cursor = conn.cursor()
-        sql = (generate_insert_sql(mysqlurl[5],times))
+        sql = (generate_insert_sql(mysqlurl[5], times))
         cursor.execute(sql)
         conn.commit()
         conn.close()
         cursor.close()
         # 可插入多条
-        
-    
-    def createTabBySql(self,mysqlurl="10.236.101.17/3306/root/123456/performancedb"):
+
+    def createTabBySql(self, mysqlurl="10.236.101.17/3306/root/123456/performancedb"):
         '''此函数只运行一次，用来创建表（数据库在连接之前去服务器创建了）'''
-        import pymysql
         mysqlurl = mysqlurl.split("/")
-        conn = pymysql.connect(host=mysqlurl[0],port=int(mysqlurl[1]),user=mysqlurl[2],passwd=mysqlurl[3],db=mysqlurl[4],charset='utf8')
+        conn = pymysql.connect(host=mysqlurl[0], port=int(mysqlurl[1]), user=mysqlurl[2], passwd=mysqlurl[3],
+                               db=mysqlurl[4], charset='utf8')
         cursor = conn.cursor()
         tb1_sql = '''CREATE TABLE IF NOT EXISTS TB_performance2(
                    redirctTime int(11) DEFAULT NULL ,
@@ -114,28 +127,43 @@ class TestJS: #https://www.jianshu.com/p/7b760e2db555
                    TestingTime datetime DEFAULT CURRENT_TIMESTAMP, 
                    updateTime datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
                    id INT NOT NULL AUTO_INCREMENT,
-                   PRIMARY KEY(id)) ''' #还可以加datatime字段
-        
-        
-        cursor.execute(tb1_sql) #哪个没有建哪个
+                   PRIMARY KEY(id)) '''  # 还可以加datatime字段
+
+        cursor.execute(tb1_sql)  # 哪个没有建哪个
         conn.commit()
         conn.close()
         cursor.close()
-        
+
+    def getMainKey(self,mysqlurl="10.236.101.17/3306/root/123456/performancedb/TB_service"):
+        mysqlurl = mysqlurl.split("/")
+        conn = pymysql.connect(host=mysqlurl[0], port=int(mysqlurl[1]), user=mysqlurl[2], passwd=mysqlurl[3],
+                               db=mysqlurl[4])
+        sqlGetMainKey = 'select id from TB_service'
+        df = pd.read_sql(sqlGetMainKey, con=conn)
+        key_list=np.array(df).tolist()
+        return key_list
+
+    def getPerformanceWithPixedTime(self,mainKey_to_list):   #可定义脚本抓取性能数据的时间
+        mainKey_to_list=mainKey_to_list
+        count = 0
+        i = 0
+        period = 7200  # 定时7200s(两小时)
+        start_time = time.time()
+        while time.time() - start_time <= period:
+            try:
+                ts.performance2mysql(
+                    url='https://zwykb.cq.gov.cn/ggbf_search/ljbl/?mainKey=' + ",".join(mainKey_to_list[i]) + '&type=01&parentPage=1')
+            except:
+                count += 1
+                print("err")
+            i = (i + 1) % 851
+        print(count)
 
 if __name__ == "__main__":
     ts = TestJS()
     ts.setup()
-    ts.createTabBySql() # 第一个链接有字段为负存不了
-    #ts.performance2mysql(url='https://zwykb.cq.gov.cn/ggbf_search/ljbl/?mainKey=aa4fc5f3-36c0-4c8a-9ac1-fbaaa2ebc8e1&type=08&parentPage=1')
-    #ts.performance2mysql(url='https://zwykb.cq.gov.cn/ggbf_search/ljbl/?mainKey=f4943a42-961d-47db-a86c-b1ada64370ab&type=01&parentPage=1')
-    count = 0
-    for i in range(200):
-        try:
-            ts.performance2mysql(url='https://zwykb.cq.gov.cn/ggbf_search/ljbl/?mainKey=f4943a42-961d-47db-a86c-b1ada64370ab&type=01&parentPage=1')
-        except:
-            count += 1
-            print("err")
-    #
-    
-    print(count)
+    mainKey_list = ts.getMainKey()
+    # ts.createTabBySql()  # 第一个链接有字段为负存不了
+    ts.getPerformanceWithPixedTime(mainKey_list)
+
+
